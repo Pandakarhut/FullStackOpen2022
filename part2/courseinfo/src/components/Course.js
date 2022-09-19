5bc2e35115669748a1f84e6faa65994683ee31b5
Jing <computer@MacBook-Air.local>
2022-09-19 21:51:30 +0300

    part2 courseInfo

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Course = ({ course }) => {
+	const initial = 0;
+	const sum = course.parts.reduce(
+		(preVal, curVal) => preVal + curVal.exercises,
+		initial
+	);
+
+	return (
+		<div>
+			<h2>{course.name}</h2>
+			<ul>
+				{course.parts.map((part) => (
+					<li key={part.id}>
+						{part.name} {part.exercises}
+					</li>
+				))}
+				<li>
+					<strong>total of {sum} exercises</strong>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Course
