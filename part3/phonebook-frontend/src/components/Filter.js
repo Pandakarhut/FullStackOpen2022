dac132a412aef4a651824b6a3053c102656f681e
Christina Tian <christina.tian@zevoy.com>
2025-05-27 16:38:04 +0300

    backend folder alone

--- part3/phonebook-frontend/src/components/Filter.js
+++
@@ -1,7 +0,0 @@
-const Filter = ({onChange}) => {
-	return (
-		<p>filter shown with <input onChange={onChange}/></p>
-	)
-  }
-
-  export default Filter
--- part3/phonebook-frontend/src/components/Persons.js
+++
@@ -1,9 +0,0 @@
-const Persons = ({ person }) => {
-  return (
-      <li>
-	  	{person.name} {person.number}
-	  </li>
-  )
-}
-
-export default Persons
